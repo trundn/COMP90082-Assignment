d1278c8d3c044e72f73186ec95e0809188d1ef5c
changhair <changhair@student.unimelb.edu.au>
2021-03-31 00:20:30 +1100

    Add image url model

--- src/server/src/models/academicModels.ts
+++ src/server/src/models/academicModels.ts
@@ -3,3 +3,7 @@ import { prop, getModelForClass } from '@typegoose/typegoose';
 class UserName {
   @prop({ required: true }) name!: string;
 }
+
+class UserImage extends UserName {
+  @prop({ required: true }) imageUrl!: string;
+}
