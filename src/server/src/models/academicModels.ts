09af6289feb407617a70f81686aef644c9abbb44
changhair <changhair@student.unimelb.edu.au>
2021-04-01 22:41:35 +1100

    add academic model and necessary variable

--- src/server/src/models/academicModels.ts
+++ src/server/src/models/academicModels.ts
@@ -12,3 +12,32 @@ class SingalImage extends UserName {
   @prop({ required: true })
   academicID!: string;
 }
+
+class Academic extends UserName {
+  @prop({ required: true })
+  academicID!: string;
+
+  @prop({ required: true })
+  title!: string;
+
+  @prop({ required: true })
+  author!: string;
+
+  @prop()
+  orginzation!: string;
+
+  @prop({ required: true })
+  createDate!: Date;
+
+  @prop({ required: true })
+  shortDescription!: string;
+
+  @prop({ required: true })
+  bodyParagraph!: string;
+
+  @prop({ required: true })
+  academicImage!: string;
+
+  @prop({ required: true })
+  academicReference!: string;
+}
