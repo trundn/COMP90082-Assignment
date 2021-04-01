f6f60586427e9767067c09f32661a467781cb17f
changhair <changhair@student.unimelb.edu.au>
2021-04-01 22:34:23 +1100

    Update image class and change class name

--- src/server/src/models/academicModels.ts
+++ src/server/src/models/academicModels.ts
@@ -1,9 +1,14 @@
 import { prop, getModelForClass } from '@typegoose/typegoose';
 
 class UserName {
-  @prop({ required: true }) name!: string;
+  @prop({ required: true })
+  name!: string;
 }
 
-class UserImage extends UserName {
-  @prop({ required: true }) imageUrl!: string;
+class SingalImage extends UserName {
+  @prop({ required: true })
+  imageUrl!: string;
+
+  @prop({ required: true })
+  academicID!: string;
 }
