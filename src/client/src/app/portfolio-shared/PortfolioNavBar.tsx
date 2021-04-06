479efdd4b59075cb9374fe1bbe2e8c07b8364d9f
changhair <changhair@student.unimelb.edu.au>
2021-04-06 20:23:59 +1000

    add academic link on navigation bar

--- src/client/src/app/portfolio-shared/PortfolioNavBar.tsx
+++ src/client/src/app/portfolio-shared/PortfolioNavBar.tsx
@@ -47,6 +47,9 @@ const PortfolioNavBar = () => {
           <LinkContainer to={`${URL_PREFIX}/about`}>
             <Nav.Link eventKey="/about">About</Nav.Link>
           </LinkContainer>
+          <LinkContainer to={`${URL_PREFIX}/academic`}>
+            <Nav.Link eventKey="/academic">Academic</Nav.Link>
+          </LinkContainer>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
